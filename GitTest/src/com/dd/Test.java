997ed793983e6902ec69d48324804ed21ca300bf
fzcss <837233741@qq.com>
2019-07-25 15:56:57 +0800

    在yyh分支上添加了dd方法

--- GitTest/src/com/dd/Test.java
+++ GitTest/src/com/dd/Test.java
@@ -8,6 +8,8 @@ package com.dd;
  * @date 2019年7月25日 下午3:37:12
  * @param 
  * @return
+ * ↑1: 有一次提交没有提交到远程仓库
+ *  分支: 一个项目至少有一个分支，这个分支叫做master
  */
 public class Test {
 	public static void main(String[] args) {
@@ -18,5 +20,8 @@ public class Test {
 	}
 	public void get(){
 		System.out.println("ppp");
+	}
+	public void dd(){
+		System.out.println("gg");
 	}
 }
